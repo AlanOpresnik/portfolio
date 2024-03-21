289fee549da009da5d3b921607936589ba5a02a6
Alan opresnik <alanopk328@gmail.com>
2024-03-21 17:36:20 -0300

    details

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center py-2'>
         <p>Hecho con ❤️ por Alan Opresnik</p>
     </div>
   )
