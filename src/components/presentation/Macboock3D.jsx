efe97505e5bb39db78fbae57ac8235f2a417d01a
Alan opresnik <alanopk328@gmail.com>
2024-03-23 17:42:27 -0300

    optimization

--- src/components/presentation/Macboock3D.jsx
+++ src/components/presentation/Macboock3D.jsx
@@ -12,7 +12,7 @@ export default function App() {
             setIsMobile(true); // Establecer isMobile en true si es un dispositivo móvil
             setLoading(false); // Establecer loading en false para evitar la carga de Spline
         }
-        
+
     }, []);
 
     // Función para manejar el evento onLoad
@@ -22,20 +22,13 @@ export default function App() {
 
     return (
         <>
-            <span className={`loader flex justify-center items-center ${loading === true ? "" : "hidden"}`}></span>
 
-            {!isMobile && (
-                <Spline
-                    className='mb-4 hidden md:block mt-12 mr-6 h-[380px] md:h-[480px] md:w-[400px] md:mt-0 '
-                    scene="https://prod.spline.design/7BPVyxmY8Hx-9pHZ/scene.splinecode"
-                    onLoad={handleSplineLoad}
-                    onAbort={handleSplineLoad}
-                />
-            )}
 
-            <div className='relative flex justify-center  md:hidden h-[400px] w-full'>
-                <div className='mr-6 relative z-10 w-full'>
-                    <img className='mb-4 block md:hidden absolute top-[-120px]  h-[730px]  w-full object-cover md:mt-0 ' src='/3dMobile.avif' />
+
+
+            <div className='relative flex justify-center   h-[400px] w-full'>
+                <div className='mr-6 relative z-10 w-[200px] h-[430px] '>
+                    <img className='mb-4 block   h-full  w-full object-cover md:mt-0 ' src='/3dMobile.avif' />
                 </div>
             </div>
         </>
